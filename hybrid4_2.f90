bf29e5c2349699541096881bbf4456acbe4b94d3
adfriend45 <adf10@cam.ac.uk>
2020-11-06 12:44:13 +0000

    Output for RINGS.

--- hybrid4_2.f90
+++ hybrid4_2.f90
@@ -3,7 +3,7 @@ program hybrid4_2
 !---------------------------------------------------------------------------------------------------------------------------------------!
 
 !---------------------------------------------------------------------------------------------------------------------------------------!
-! Learn from /home/adf10/MODELS/HYBRID10 on CSD3 for how to ue TRENDY forcings.
+! Learn from /home/adf10/MODELS/HYBRID10 on CSD3 for how to use TRENDY forcings.
 ! FW00 is Friend AD, White A. 2000. Eval...
 !---------------------------------------------------------------------------------------------------------------------------------------!
 
@@ -24,7 +24,13 @@ real, parameter :: fillvalue = 1.0e20
 real, parameter :: tf = 273.15
 real, parameter :: eps = 1.0e-8
 real, parameter :: zero = 0.0
-character (len = 250) :: file_name ! Generic filename.
+! Uggla site.
+real, parameter :: lon_w = 19.0 + 46.0 / 60.0
+real, parameter :: lat_w = 64.0 + 21.0 / 60.0
+! Grudd site.
+!real, parameter :: lon_w = 19.75
+!real, parameter :: lat_w = 68.25
+character (len = 250) :: file_name ! Generic filename
 character (len = 100) :: var_name
 character (len =     4) :: char_year
 integer :: kyr
@@ -37,20 +43,23 @@ integer :: i
 integer :: j
 integer :: k
 integer :: nland
+integer :: i_w
+integer :: j_w
 real, allocatable, dimension (:,:,:) :: tmp ! K
 real, allocatable, dimension (:,:,:) :: pre ! mm 6hr-1
-real, allocatable, dimension (:,:,:) ::spfh ! kg kg-1
-real, allocatable, dimension (:,:,:) ::pres ! Pa
+real, allocatable, dimension (:,:,:) :: spfh ! kg kg-1
+real, allocatable, dimension (:,:,:) :: pres ! Pa
 real, allocatable, dimension (: ) :: lat ! degrees north
-real :: isc ! Initial total soil C (kg[C] m-2).
-real :: t ! Temperature (degree C).
-real :: t_d ! Dew-point (degree C).
-real :: eo ! Penman evaporation from lake (mm day-1).
-real :: pt ! Annual precipiation (m yr-1).
-real :: vap ! Vapour pressure (Pa).
-real :: e ! Vapour pressure (mbar).
-real :: m_air = 28.9647 ! Molecular weight of dry air (kg [air] mol-1).
-real :: m_water = 18.01528 ! Molecular weight of water (kg [water] mol-1).
+real, allocatable, dimension (:) :: lon ! degrees east
+real :: isc ! Initial total soil C (kg[C] m-2)
+real :: t ! Temperature (degree C)
+real :: t_d ! Dew-point (degree C)
+real :: eo ! Penman evaporation from lake (mm day-1)
+real :: pt ! Annual precipiation (m yr-1)
+real :: vap ! Vapour pressure (Pa)
+real :: e ! Vapour pressure (mbar)
+real :: m_air = 28.9647 ! Molecular weight of dry air (kg [air] mol-1)
+real :: m_water = 18.01528 ! Molecular weight of water (kg [water] mol-1)
 !---------------------------------------------------------------------------------------------------------------------------------------!
 
 !---------------------------------------------------------------------------------------------------------------------------------------!
@@ -58,12 +67,22 @@ allocate (tmp (nlon,nlat,ntimes))
 allocate (pre (nlon,nlat,ntimes))
 allocate (spfh (nlon,nlat,ntimes))
 allocate (pres (nlon,nlat,ntimes))
+allocate (lon(nlon))
 allocate (lat (nlat))
 !---------------------------------------------------------------------------------------------------------------------------------------!
 
 !---------------------------------------------------------------------------------------------------------------------------------------!
+! Gridbox indices for containing wanted site in climate data.
+!---------------------------------------------------------------------------------------------------------------------------------------!
+i_w = nint ((719.0 / 2.0) * (1.0 + lon_w / 179.75)) + 1
+j_w = nint ((359.0 / 2.0) * (1.0 + lat_w /   89.75)) + 1
+write(*,*) 'i_w j_w ',i_w,j_w
+if (lat_w == (64.0 + 21.0 / 60.0)) open (20,file='uggla.clm',status='unknown')
+!---------------------------------------------------------------------------------------------------------------------------------------!
+
+!---------------------------------------------------------------------------------------------------------------------------------------!
 syr = 1901
-eyr = 1901
+eyr = 2019
 !---------------------------------------------------------------------------------------------------------------------------------------!
 
 !---------------------------------------------------------------------------------------------------------------------------------------!
@@ -82,8 +101,10 @@ do kyr = syr, eyr
  &//TRIM(var_name)//'.'//char_year//'.365d.noc.nc'
  write (*,*) 'Reading from ',trim(file_name)
  call check (nf90_open (trim (file_name), nf90_nowrite, ncid))
- ! If first year, get lats (start at SP; degrees north).
+ ! If first year, get lats (start at SP; degrees north) and lons.
  if (kyr == syr) then
+  varid = 2
+  call check (nf90_get_var (ncid, varid, lon))
   varid = 3
   call check (nf90_get_var (ncid, varid, lat))
  end if
@@ -132,8 +153,15 @@ do kyr = syr, eyr
  call check (nf90_close (ncid))
  !--------------------------------------------------------------------------------------------------------------------------------------!
  
-! If first year, set total soil C using relationship in Fig. 2 of FW00, given by Eqn. 3.
-! Annual potential evaporation, from Linacre (1977)? (m yr-1).
+ !--------------------------------------------------------------------------------------------------------------------------------------!
+ ! Climate output if needed.
+ do it = 1, ntimes
+  write (20,'(2i5,f12.4,f12.5)') kyr,it,tmp(i_w,j_w,it),pre(i_w,j_w,it)
+ end do
+ !--------------------------------------------------------------------------------------------------------------------------------------!
+
+ ! If first year, set total soil C using relationship in Fig. 2 of FW00, given by Eqn. 3.
+ ! Annual potential evaporation, from Linacre (1977)? (m yr-1).
  if (kyr == syr) then
   do j = 1, nlat
    do i = 1, nlon
@@ -163,11 +191,12 @@ do kyr = syr, eyr
      else
       isc = 10.8 - 1.6 * eo / pt
      end if ! eo / pt
-     write(*,*)lat(j),pt,eo,isc
+     !write(*,*)lat(j),pt,eo,isc
     end if ! fillvalue
    end do ! i
   end do ! j
  end if ! kyr == syr
+ close (20) ! Climate output if needed.
  
  !--------------------------------------------------------------------------------------------------------------------------------------!
  ! Loop through gridboxes and integrate state variables at land points.
@@ -185,7 +214,7 @@ do kyr = syr, eyr
  end do ! i
  !--------------------------------------------------------------------------------------------------------------------------------------!
 
-end do
+end do ! kyr = syr, eyr
 !---------------------------------------------------------------------------------------------------------------------------------------!
 
 !---------------------------------------------------------------------------------------------------------------------------------------!
